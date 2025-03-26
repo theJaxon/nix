2a5264c2a371b062c8a3746a46d102b604db73d4
Jaxon <mohamedmostels@gmail.com>
2025-03-26 13:36:00 +0100

    Use home-manager

--- flake.nix
+++ flake.nix
@@ -1,66 +1,94 @@
 {
   description = "Jaxon nix-darwin system flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, home-manager, nix-darwin, nixpkgs, firefox-addons }:
   let
+    username = "jaxon";
     configuration = { pkgs, ... }: {
-      nixpkgs.config.allowUnfree = true;
-      environment.shells = [ pkgs.zsh ];
-      environment.systemPackages = with pkgs; [
-        aerospace
-        ansible
-        alacritty
-        awscli2
-        fluxcd
-        go
-        hugo
-        helix
-        jankyborders
-        kubernetes-helm
-        logseq
-        nixfmt-rfc-style
-        nodePackages.aws-cdk
-        nushell
-        packer
-        qemu
-        rar
-        terraform
-        vim
-        vscode
-        yq-go
-      ];
-
-      fonts.packages = with pkgs; [nerd-fonts.jetbrains-mono];
-      programs.zsh.enable = true;
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      nixpkgs.hostPlatform = "aarch64-darwin";
-      security.pam.enableSudoTouchIdAuth = true;
-
-      system.defaults = {
-        dock.autohide = true;
-        dock.mru-spaces = false;
-        finder.AppleShowAllExtensions = true;
+    users = {
+        users.${username} = {
+          name = "${username}";
+          home = "/Users/${username}";
+        };
       };
+    nixpkgs.config.allowUnfree = true;
+    environment.shells = [ pkgs.zsh ];
+    environment.systemPackages = with pkgs; [
+      aerospace
+      ansible
+      awscli2
+      firefox
+      git
+      go
+      google-chrome
+      hugo
+      jankyborders
+      nixfmt-rfc-style
+      nodePackages.aws-cdk
+      packer
+      rar
+      terraform
+      vim
+      yq-go
+    ];
+
+    fonts.packages = with pkgs; [nerd-fonts.jetbrains-mono];
+    programs.zsh.enable = true;
+    nix.settings.experimental-features = "nix-command flakes";
+
+    # Set Git commit hash for darwin-version.
+    system.configurationRevision = self.rev or self.dirtyRev or null;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 5;
+
+    nixpkgs.hostPlatform = "aarch64-darwin";
+    security.pam.services.sudo_local.touchIdAuth = true;
+
+    system.defaults = {
+      dock.autohide = true;
+      dock.mru-spaces = false;
+      finder.AppleShowAllExtensions = true;
     };
+  };
+
+  homeconfig = {pkgs, ...}: {
+    home.stateVersion = "24.11";
+    programs.home-manager.enable = true;
+    home.packages = with pkgs; [];
+  };
+
   in
   {
-    # $ darwin-rebuild build --flake .#mac
     darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        home-manager.darwinModules.home-manager 
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = { inherit inputs; };
+            users.${username} = import ./home.nix;
+          };
+        }
+      ];
     };
   };
 }
