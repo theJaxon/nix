6a142bda225dc3fa120a257189724ca36c7072d0
Jaxon <mohamedmostels@gmail.com>
2025-03-27 21:10:01 +0100

    Update vscode + firefox settings and add starship

--- home.nix
+++ home.nix
@@ -21,40 +21,72 @@ in
           "editor.fontLigatures" = true;
           "editor.fontSize" = font-size;
           "editor.fontFamily" = "${font-family}";
+          "editor.tabSize" = 2;
           "terminal.integrated.fontFamily" = "${font-family}";
           "workbench.colorTheme" = "Nord";
           "workbench.iconTheme" = "catppuccin.catppuccin-vsc-icons";
+          "workbench.startupEditor" = "none";
+          "security.workspace.trust.enabled" = false;
+          "security.workspace.trust.startupPrompt" = "never";
+          "security.workspace.trust.banner" = "never";
+          "update.showReleaseNotes" = false;
         };
       };
     };
 
     alacritty = {
-        enable = true;
-        package = pkgs.alacritty;
-        settings = {
-      font = {
-				normal = {
-					family = "${font-family}";
-					style = "Regular";
-				};
-				bold = {
-					family = "${font-family}";
-					style = "Bold";
-				};
-				italic = {
-					family = "${font-family}";
-					style = "Italic";
-				};
-        size = font-size;
-      };
-      selection.save_to_clipboard = true;
+      enable = true;
+      package = pkgs.alacritty;
+      settings = {
+        font = {
+				  normal = {
+					  family = "${font-family}";
+					  style = "Regular";
+				  };
+				  bold = {
+					  family = "${font-family}";
+					  style = "Bold";
+				  };
+				  italic = {
+					  family = "${font-family}";
+					  style = "Italic";
+				  };
+          size = font-size;
         };
+        selection.save_to_clipboard = true;
+      };
     };
+
     firefox = {
       enable = true;
       package = pkgs.firefox;
       profiles.jaxon = {
         extensions.packages = with inputs.firefox-addons.packages."aarch64-darwin"; [ublock-origin];
+        settings = {
+          extensions.pocket.enabled = false;
+          # Disable firefox login
+          identity.fxaccounts = {
+            enabled = false;
+            toolbar.enabled = false;
+            pairing.enabled = false;
+          };
+          # Disable tutorial
+          browser.uitour.enabled = false;
+          browser.newtabpage.activity-stream.showSponsored = false;
+          browser.newtabpage.activity-stream.showSponsoredTopSites = false;
+        };
+      };
+    };
+
+    # starship - a customizable prompt for any shell
+    starship = {
+      enable = true;
+      settings = {
+        add_newline = false;
+        enableZshIntegration = true;
+        aws.disabled = true;
+        gcloud.disabled = true;
+        line_break.disabled = true;
       };
     };
   };
